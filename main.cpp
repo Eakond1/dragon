5ccf371d7e2d66d15f9c60ae42acd61bb3d4b2d7
Eakond1 <143996703+Eakond1@users.noreply.github.com>
2023-09-11 09:40:13 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,63 @@
+
+#include "easy.h"
 #include <iostream>
-#include "header.h"
 using namespace std;
 
 int main()
 {
+   /* itc_name();
+    itc_fio();
+    int num;
+    cin>> num;
+    cout<<itc_abs(num);
+    double a;
+    cin>> a;
+    cout<< itc_fabs(a);
+    int b;
+    cin>>b;
+    itc_revnbr(b);
+    int c;
+    cin>>c;
+    cout<< itc_iseven(c);
+    int d,e;
+    cin>>d>>e;
+
+    cout<<itc_max(d,e);
+    int min1,min2;
+    cin>> min1>>min2;
+    cout<<itc_min(min1,min2);
+
+    double num,num2;
+    cin>>num>>num2;
+
+    cout<<itc_fmax(num,num2);
+    double num,num2;
+    cin>>num>>num2;
+
+    cout<<itc_fmin(num,num2);
+    int num;
+    cin>>num;
+    cout<<itc_sqrt(num);
+    int num;
+    cin>>num;
+    cout<<itc_skv( num);
     int a,b;
     cin>>a>>b;
-    cout << summ(a,b) << endl;
+    cout<<itc_spr(a,  b);
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<< itc_str( a,  b, c);
+    double r;
+    cin>>r;
+    cout<< itc_scir(r);
+    int num,step;
+    cin>>num>>step;
+    cout<< itc_pow(num, step);
+    int num;
+    cin>>num;
+    cout<< itc_ispositive( num);*/
+        int num;
+    cin>>num;
+    cout<< itc_ispositive_d( num);
     return 0;
 }
